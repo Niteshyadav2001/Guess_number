723ced14b0015995643cb05b611d60a3e203e637
Niteshyadav2001 <nyrao0017@gmail.com>
2023-12-30 02:52:22 -0800

    files added

---
+++ script1.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let num = Math.trunc(Math.random()*20)+1;
+
+
+document.querySelector('.check').addEventListener('click',function()
+{
+    const guess = Number(document.querySelector('.guess').value);
+    let score = Number(document.querySelector('.score').textContent);
+    if(!guess)
+    {
+        document.querySelector('.message').textContent = '❌NO NUMBER!';
+    }
+    else if(guess > num && guess <= 20)
+    {
+        if(score >= 1)
+        {
+            document.querySelector('.message').textContent = '😒TOO HIGH!';
+            --score;
+            document.querySelector('.number').textContent = '?';
+            document.querySelector('.score').textContent = String(score);
+        }
+        else{
+            document.querySelector('.message').textContent = '💥 YOU LOST THE GAME!';
+        }
+        
+    }
+    else if(guess < num && guess >= 1)
+    {
+        if(score >= 1)
+        {
+            document.querySelector('.message').textContent = '😒TOO LOW!';
+            --score;
+            document.querySelector('.number').textContent = '?';
+            document.querySelector('.score').textContent = String(score);
+        }
+        else{
+            ocument.querySelector('.message').textContent = '💥 YOU LOST THE GAME!';
+        }
+    }
+    else if(guess === num)
+    {
+        let highscore = Number(document.querySelector('.highscore').textContent);
+        if(score > highscore)
+        {
+            document.querySelector('.highscore').textContent = String(score);
+        }
+        document.querySelector('.number').textContent = num;
+        document.querySelector('.message').textContent = '😁YOU GOT IT!';
+    }
+    else if(guess > 20)
+    {
+        document.querySelector('.message').textContent = '😑CHOOSE B/W 1 to 20!';
+    }
+    else if(guess < 1)
+    {
+        document.querySelector('.message').textContent = '😑CHOOSE B/W 1 to 20!';
+    }
+});
+
+
+
+//add event listener to "AGAIN BUTTON"
+document.querySelector('.again').addEventListener('click',function(){
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = '20';
+    document.querySelector('.number').textContent = '?';
+    num = Math.trunc(Math.random()*20)+1;
+    
+});
